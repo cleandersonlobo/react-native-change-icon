dddfd0d2d2894f261ed43432c6528ec77ccbaa33
Cleanderson Lobo <cleandersonlobo@gmail.com>
2020-09-04 12:44:53 -0300

    feat: getCurrentIcon ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { NativeModules } from 'react-native';
 
-const changeIcon = (iconName: string) =>
+const changeIcon = (iconName: string): Promise<string| null |undefined> =>
   NativeModules.ChangeIcon.changeIcon(iconName);
+const getCurrentIcon = (): Promise<string| null |undefined> => NativeModules.ChangeIcon.getCurrentIcon();
 
-export { changeIcon };
+export { changeIcon, getCurrentIcon };
